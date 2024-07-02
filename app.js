d6c97abd1228f5dceb40df8ff51f07d5b3626862
Val-Ge <vg.dataengineer@gmail.com>
2024-07-02 14:09:56 +0100

    added authentication with passport

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const user = require('./models/user')
+
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
-// const contactRoutes = require('./routes/contact');
-// const aboutRoutes = require('./routes/about');
-// const postRoutes = require('./routes/post');
+// Middleware for parsing POST request bodies
+app.use(express.urlencoded({ extended: true }));
+
+//Middleware for sessions
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Initialize Passport and Session
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Passport local strategy for username/password authentication
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    User.findOne({ email: email }, (err, user) => {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false, { message: 'Incorrect email.' }); }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) { return done(err); }
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+      });
+    });
+  }));
+  
+//serialize user into session
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+//deserialize user from session
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
@@ -17,10 +63,7 @@ app.set('layout', 'layouts/boilerplate'); // Specify the default layout file
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// app.use('/about', aboutRoutes)
-// app.use('/contact', contactRoutes)
-// app.use('/posts', postRoutes)
-// app.use('/user', userRoutes)
+
 
 // Define routes
 app.get('/', (req, res) => {
